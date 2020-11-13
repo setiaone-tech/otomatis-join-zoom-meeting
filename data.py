9ad28f3da8dd096766693dcb67400a852b2af9d1
bang L <71912420+setiaone-tech@users.noreply.github.com>
2020-11-13 19:09:31 +0700

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,12 @@
 import pandas as pd
+from datetime import datetime
 
 df = pd.read_csv('jadwal.csv', delimiter=',')
 
-lst = [list(row) for row in df.values]
+a = [list(row) for row in df.values]
+lst = []
+
+for i in a:
+    while True:
+        if datetime.now().strftime("%a") == i[0] and datetime.now().hour == int(i[1].split(':')[0]) and datetime.now().minute == int(i[1].split(':')[1]):
+            lst.append(i)
